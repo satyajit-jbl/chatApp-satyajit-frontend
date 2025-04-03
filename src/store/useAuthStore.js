21aeec1c337aa5c45844ae73eee7c162fbf07fc1
satyajit <satyajit_jbl@yahoo.com>
2025-04-04 01:16:39 +0600

    profile page  done and working properly

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -61,4 +61,18 @@ export const useAuthStore = create((set)=>({
         }
     },
 
+    updateProfile: async (data) =>{
+        set({isUpdatingProfile: true});
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({authUser: res.data})
+            toast.success("Profile Updated Successfully")
+        } catch (error) {
+            console.log("error in update profile", error);
+            toast.error(error.response.data.message);
+        }finally{
+            set({isUpdatingProfile: false})
+        }
+    },
+
 }));
