d264fa3fa242bbee6dc714cd2df509d92432b35a
satyajit <satyajit_jbl@yahoo.com>
2025-04-04 23:55:30 +0600

    send messages successfully to db done

--- src/store/useChatStore.js
+++ src/store/useChatStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
 
-export const useChatStore = create((set)=>({
+export const useChatStore = create((set, get)=>({
     message: [],
     users: [],
     selectedUser: null,
@@ -26,7 +26,7 @@ export const useChatStore = create((set)=>({
     getMessages: async (userId) =>{
         set({isMessagesLoading: true});
         try {
-            const res = axiosInstance.get(`messages/${userId}`);
+            const res = await axiosInstance.get(`messages/${userId}`);
             set({messages: res.data});
         } catch (error) {
             toast.error(error.response.data.message)
@@ -36,5 +36,18 @@ export const useChatStore = create((set)=>({
 
    },
 
+   sendMessage: async(messageData)=>{
+    const {selectedUser, messages} = get(); 
+    try {
+        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+        set({messages: [...messages, res.data]});
+    } catch (error) {
+        toast.error(error.response.data.message);
+    }
+   },
+
+
+
+   // todo: optimize thin one later
    setSelectedUser: (selectedUser)=>set({selectedUser}),
 }));
